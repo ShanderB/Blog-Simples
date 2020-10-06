df121e3c6f381563bf8296a892467ff3c3a5e915
ShanderB <s.hander8910@hotmail.com>
2020-10-06 17:28:15 -0300

    Inicial Auth

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const Postagem = mongoose.model("postagens");
 require("./models/Categoria");
 const Categoria = mongoose.model("categorias");
 const usuarios = require("./routes/usuario")
+const passport = require("passport");
+require("./config/auth")(passport)
 
 //configs 
 
@@ -22,6 +24,8 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 
 // middleWare
